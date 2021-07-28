e9f745462fe862d2e23857f0fe0cd0b8af737389
dani serven <dserven@gmail.com>
2021-07-28 21:03:39 +0200

    setup-py installs dependencies

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,4 +1,19 @@
 from setuptools import setup, find_namespace_packages
+from pathlib import Path
+
+
+def parse_requirements():
+    def remove_non_reqs(line):
+        return not (line.startswith("#") or line.startswith("-e")) and bool(line)
+
+    reqs_file = list(Path(__file__).resolve().parent.glob("requirements.txt"))
+    if reqs_file:
+        reqs = reqs_file[0].read_text().split("\n")
+        reqs = list(filter(remove_non_reqs, reqs))
+        return reqs
+
+    return []
+
 
 setup(
     name='{{ "conny-ml-" + cookiecutter.package_name.replace(" ", "-").replace("_", "-") }}',
@@ -8,4 +23,5 @@ setup(
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+    install_requires=parse_requirements()
 )
